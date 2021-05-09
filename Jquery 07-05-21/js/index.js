4ae87777f9d5809602b96423eb8c133503280530
Pavankumar44 <pavan99424@gmail.com>
2021-05-09 16:48:38 +0530

    work

---
+++ Jquery 07-05-21/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('button').click(()=> {
+        $("p").toggle();
+    });
+    /*$('#show').click(()=> {
+        $("p").show();
+    });*/
+    /*$('#hide').click(()=> {
+        $("p").hide();
+    });*/
+    //$("p").addClass('add-color');
+});
